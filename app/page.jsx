29b61ec1c9d3854f5e413dbaf3cb0e302ca68ad9
muhammadubaydillah10 <muhammadubaydillah662@gmail.com>
2025-12-12 18:01:49 +0700

    Add home page

---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Upload Video ke Supabase</h1>
+      <p>Website sudah aktif! Nanti di sini kita buat form upload.</p>
+    </div>
+  );
+}
